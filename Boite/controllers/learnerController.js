b4abcb94e08ce7a27b074bf288cd056c0bcf9a7d
jeannot diambu <diambujeannot@gmail.com>
2023-01-14 16:33:32 +0100

    add learner update method for learner

--- Boite/controllers/learnerController.js
+++ Boite/controllers/learnerController.js
@@ -82,3 +82,80 @@ export const getOnLearner = async (req, res) => {
     return new Error(err);
   }
 };
+
+export const deleteLearner = async (req, res) => {
+  const id = req.query.id;
+  try {
+    const learner = await learnerModel.findByIdAndRemove(id);
+    if (!learner) {
+      res.status(500).json({ message: "Unable to delete" });
+    } else {
+      res.status(200).json({ message: "Learner Deleted" });
+    }
+  } catch (err) {
+    return new Error(err);
+  }
+};
+
+export const putLearner = async (req, res) => {
+  const id = req.query.id;
+  console.log(id);
+  const {
+    nom,
+    postnom,
+    prenom,
+    sexe,
+    image,
+    email,
+    password,
+    contact,
+    filiere,
+    description,
+    entreprise,
+  } = req.body;
+
+  if (
+    !nom &&
+    nom.trim() === "" &&
+    !postnom &&
+    postnom.trim() === "" &&
+    !prenom &&
+    prenom.trim() === "" &&
+    !sexe &&
+    sexe.trim() === "" &&
+    !image &&
+    image.trim() === "" &&
+    !email &&
+    email.trim() === "" &&
+    !password &&
+    password.trim() === "" &&
+    !filiere &&
+    filiere.trim() === ""
+  ) {
+    res.status(422).json({ message: "Invalid Input" });
+  }
+
+  try {
+    const learner = await learnerModel.findByIdAndUpdate(id, {
+      nom,
+      postnom,
+      prenom,
+      sexe,
+      image,
+      email,
+      password,
+      contact,
+      filiere,
+      description,
+      entreprise,
+    });
+
+    if (!learner) {
+      res.status(500).json({ message: "Interval Server Error" });
+    } else {
+      res.status(200).json({ learner, message: "Successfull" });
+    }
+  } catch (err) {
+    return new Error(err);
+  }
+};
--- pages/api/learner/[id].js
+++ pages/api/learner/[id].js
@@ -1,9 +1,19 @@
 import connectMongoDB from "../../../Boite/connexionDB/connectMongoDB";
-import { getOnLearner } from "../../../Boite/controllers/learnerController";
+import {
+  deleteLearner,
+  getOnLearner,
+  putLearner,
+} from "../../../Boite/controllers/learnerController";
 
 export default async function handler(req, res) {
   await connectMongoDB();
-    if (req.method === "GET") {
-      return getOnLearner(req, res);
-    }
+  if (req.method === "GET") {
+    return getOnLearner(req, res);
+  }
+  if (req.method === "DELETE") {
+    return deleteLearner(req, res);
+  }
+  if (req.method === "PUT") {
+    return putLearner(req, res);
+  }
 }
