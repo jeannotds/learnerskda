89b3b45ba97542c0b3b956c5a649119e5710471b
jeannot diambu <diambujeannot@gmail.com>
2023-01-24 15:30:04 +0100

    Add : add file Header.js

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Header() {
+  return (
+    <header className='header'>
+        <a href='#'>Accueil</a>
+        <a href='#'>Accueil</a>
+        <a href='#'>Accueil</a>
+        <a href='#'>Accueil</a>
+        <a href='#'>Accueil</a>
+    </header>
+  )
+}
+
+export default Header
