f1fc29b9e260a812e2107f6ad795c716b882e759
jeannot diambu <diambujeannot@gmail.com>
2023-01-25 11:48:52 +0100

    Udate : Improve learner.css

--- components/Learners.js
+++ components/Learners.js
@@ -4,7 +4,7 @@ import ImageMain from "../img/main.jpg";
 
 function Learners() {
   return (
-    <div className="learners">
+    <div className="container">
       <div className="msg-alert">
         <h2>Digital marketing specialists and web developers in Africa</h2>
         <p>
@@ -13,81 +13,97 @@ function Learners() {
           deserunt reprehenderit error ab! Eveniet a voluptatibus corrupti
         </p>
       </div>
-      <div className="learnerAleatoir">
-        <div className="container-learner">
-          <div className="blog-learner">
-            <Image
-              src={ImageMain}
-              alt="image of main page"
-              className="leaner_img"
-            />
-            <div className="title-learner">Specialiiste en Markint Digital</div>
-            <div className="learner-description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-              dignissimos eaque totam. Quidem, perferendi. perferendis.
+      <div className="learners-pub">
+        <div className="learnerAleatoir">
+          <div className="container-learner">
+            <div className="blog-learner">
+              <Image
+                src={ImageMain}
+                alt="image of main page"
+                className="leaner-img"
+              />
+              <div className="title-learner">
+                Specialiiste en Markint Digital
+              </div>
+              <div className="learner-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+                dignissimos eaque totam. Quidem, perferendi. perferendis.
+              </div>
+              <div className="detail">Detail</div>
             </div>
-            <div className="detail">Detail</div>
+          </div>
+          <div className="container-learner">
+            <div className="blog-learner">
+              <Image
+                src={ImageMain}
+                alt="image of main page"
+                className="leaner-img"
+              />
+              <div className="title-learner">
+                Specialiiste en Markint Digital
+              </div>
+              <div className="learner-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+                dignissimos eaque totam. Quidem, perferendi. perferendis.
+              </div>
+              <div className="detail">Detail</div>
+            </div>
+          </div>
+          <div className="container-learner">
+            <div className="blog-learner">
+              <Image
+                src={ImageMain}
+                alt="image of main page"
+                className="leaner-img"
+              />
+              <div className="title-learner">
+                Specialiiste en Markint Digital
+              </div>
+              <div className="learner-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+                dignissimos eaque totam. Quidem, perferendi. perferendis.
+              </div>
+              <div className="detail">Detail</div>
+            </div>
+          </div>
+          <div className="container-learner">
+            <div className="blog-learner">
+              <Image
+                src={ImageMain}
+                alt="image of main page"
+                className="leaner-img"
+              />
+              <div className="title-learner">
+                Specialiiste en Markint Digital
+              </div>
+              <div className="learner-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+                dignissimos eaque totam. Quidem, perferendi. perferendis.
+              </div>
+              <div className="detail">Detail</div>
+            </div>
+          </div>
+          <div className="container-learner">
+            <div className="blog-learner">
+              <Image
+                src={ImageMain}
+                alt="image of main page"
+                className="leaner-img"
+              />
+              <div className="title-learner">
+                Specialiiste en Markint Digital
+              </div>
+              <div className="learner-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+                dignissimos eaque totam. Quidem, perferendi. perferendis.
+              </div>
+              <div className="detail">Detail</div>
+            </div>
           </div>
         </div>
-        <div className="container-learner">
-          <div className="blog-learner">
-            <Image
-              src={ImageMain}
-              alt="image of main page"
-              className="leaner_img"
-            />
-            <div className="title-learner">Specialiiste en Markint Digital</div>
-            <div className="learner-description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-              dignissimos eaque totam. Quidem, perferendi. perferendis.
-            </div>
-            <div className="detail">Detail</div>
-          </div>
-        </div>
-        <div className="container-learner">
-          <div className="blog-learner">
-            <Image
-              src={ImageMain}
-              alt="image of main page"
-              className="leaner-img"
-            />
-            <div className="title-learner">Specialiiste en Markint Digital</div>
-            <div className="learner-description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-              dignissimos eaque totam. Quidem, perferendi. perferendis.
-            </div>
-            <div className="detail">Detail</div>
-          </div>
-        </div>
-        <div className="container-learner">
-          <div className="blog-learner">
-            <Image
-              src={ImageMain}
-              alt="image of main page"
-              className="leaner-img"
-            />
-            <div className="title-learner">Specialiiste en Markint Digital</div>
-            <div className="learner-description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-              dignissimos eaque totam. Quidem, perferendi. perferendis.
-            </div>
-            <div className="detail">Detail</div>
-          </div>
-        </div>
-        <div className="container-learner">
-          <div className="blog-learner">
-            <Image
-              src={ImageMain}
-              alt="image of main page"
-              className="leaner-img"
-            />
-            <div className="title-learner">Specialiiste en Markint Digital</div>
-            <div className="learner-description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-              dignissimos eaque totam. Quidem, perferendi. perferendis.
-            </div>
-            <div className="detail">Detail</div>
-          </div>
+        <div className="publicity">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+          dignissimos eaque totam. Quidem, perferendi. perferendis.
         </div>
       </div>
     </div>
