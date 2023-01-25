afaa1c1c65925e0a0f72b58eea0cf8662c10cd16
jeannot diambu <diambujeannot@gmail.com>
2023-01-25 18:27:52 +0100

    Improve style home

--- components/Header.js
+++ components/Header.js
@@ -3,13 +3,13 @@ import React from "react";
 function Header() {
   return (
     <header className="header">
-      <a href="#"><h1 className="logo">Logo</h1></a>
+      <a href="#" className="link logo" ><h1 className="logo">Logo</h1></a>
       <nav className="nav-link">
-        <a href="#">Accueil</a>
-        <a href="#">A propos</a>
-        <a href="#">Categorie</a>
-        <a href="#">S inscrire</a>
-        <a href="#" className="important">Connexion</a>
+        <a href="#" className="link">Accueil</a>
+        <a href="#"  className="link" >A propos</a>
+        <a href="#"  className="link">Categorie</a>
+        <a href="#"  className="link">S inscrire</a>
+        <a href="#" className="important link">Connexion</a>
       </nav>
     </header>
   );
--- components/Main.js
+++ components/Main.js
@@ -11,8 +11,6 @@ function Mains() {
             <p>Contacter un developpeur(se) web ou un marketeur(se) digital qui vous contient pour que votre entreprise soit numerisée comme vous y pensez.</p>
             <div className="inscription">{inscrire}</div>
         </div>
-        <Image src={ImageMain} alt="image of main page" className='main_img' />
-        <div className='floot'></div>
     </div>
   )
 }
