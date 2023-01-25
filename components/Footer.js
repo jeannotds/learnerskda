45e7f27fccfb8aa073d0180dd07e0f4424e6f933
jeannot diambu <diambujeannot@gmail.com>
2023-01-25 16:53:35 +0100

    Add : font in footer.css

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,52 @@
 import React from "react";
+import {
+  FaFacebookSquare,
+  FaLinkedin,
+  FaTwitterSquare,
+  FaWhatsappSquare,
+} from "react-icons/fa";
 
 function Footer() {
   return (
     <footer className="footer">
-      <div className="contact"></div>
-      <div className="categorie"></div>
-      <div className="isncription"></div>
-      <div className="apropos"></div>
+      <div className="contact">
+        <h2>Nous contacter</h2>
+        <ul>
+          <li>
+            <FaFacebookSquare className="icon"/>
+          </li>
+          <li>
+            <FaLinkedin className="icon" />
+          </li>
+          <li>
+            <FaTwitterSquare className="icon" />
+          </li>
+          <li>
+            <FaWhatsappSquare className="icon" />
+          </li>
+        </ul>
+      </div>
+      <div className="categorie">
+        <h2>Categorie</h2>
+        <ul>
+          <li>Developpement Web et Web Mobile</li>
+          <li>Marketing Digital</li>
+        </ul>
+      </div>
+      <div className="apropos">
+        <h2>A propos</h2>
+        <ul>
+          <li>Kinshasa Digital</li>
+          <li>Concepteurs</li>
+        </ul>
+      </div>
+      <div className="inscrire">
+        <h2>A propos</h2>
+        <ul>
+          <li>Kinshasa Digital Academie</li>
+          <li>Concepteurs</li>
+        </ul>
+      </div>
     </footer>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import '../styles/header.css'
 import '../styles/main.css'
 import '../styles/learners.css'
+import '../styles/footer.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
