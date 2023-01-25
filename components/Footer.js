a3e02ef971e66fbd28bee9292f1482bc54d37132
jeannot diambu <diambujeannot@gmail.com>
2023-01-25 16:04:24 +0100

    Add : add file component Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer">
+      <div className="contact"></div>
+      <div className="categorie"></div>
+      <div className="isncription"></div>
+      <div className="apropos"></div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
